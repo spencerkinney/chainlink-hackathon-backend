ad7ef4da061a995b6a81de8d9c1c5a10b97de722
spencerkinney <spencer_kinney@yahoo.com>
2023-11-27 19:39:37 -0600

    stable diff XL

--- stable_diff.py
+++ stable_diff.py
@@ -17,7 +17,7 @@ prompts = [
 # Iterate over each prompt and generate an image
 for prompt in prompts:
     output = replicate.run(
-        "stability-ai/sdxl:2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2",
+        "stability-ai/sdxl:2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2", # Model ID for Stable Diffusion XL
         input={"prompt": prompt},
     )
 
