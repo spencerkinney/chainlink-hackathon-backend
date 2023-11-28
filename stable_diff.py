c9539a2cde92a086e3e199915179d58b5d4696e2
spencerkinney <spencer_kinney@yahoo.com>
2023-11-27 19:23:12 -0600

    added a gitgnore and API key

--- stable_diff.py
+++ stable_diff.py
@@ -1,4 +1,13 @@
 import replicate
+from dotenv import load_dotenv
+
+
+# Load environment variables
+load_dotenv()
+
+
+
+
 output = replicate.run(
     "stability-ai/sdxl:2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2",
     input={"prompt": "an astronaut riding a rainbow unicorn"},
