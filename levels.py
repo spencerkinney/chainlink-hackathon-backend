0ea6675f680e2a9a4feca488e00f5dc2bf1e8ee1
spencerkinney <spencer_kinney@yahoo.com>
2023-12-08 14:43:59 -0600

    levels added

---
+++ levels.py
@@ -0,0 +1,15 @@
+weapon = ["axe", "sword", "dagger", "staff", "whip", ]
+
+item = ["healing potion", "magic potion",
+        "herbal ingredients", "cloak", "helmet", "tools", "key"]
+
+wealth = ["gold", "gems", "Silver ring", "Gold necklace"]
+
+rarity_level = {
+    "common": 0.6, # 60% chance
+    "uncommon": 0.3, # 30% chance
+    "rare": 0.09, # 9% chance
+    "legendary": 0.01, # 1% chance
+}
+
+# Examples: A common axe, A rare healing potion, A legendary gold necklace
